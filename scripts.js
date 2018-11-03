0dd3694631db483d48a786dcf2d82e245a129716
SunnaK7 <43792362+SunnaK7@users.noreply.github.com>
2018-11-03 17:45:42 +0000

    the whole shamalam

--- scripts.js
+++ scripts.js
@@ -14,37 +14,81 @@ const text = (() => {
     items = _items;
     _form.addEventListener('submit', formHandler);
 
-    // TODO láta hluti í _items virka
+  _items.addEventListener("click", (e)=> {
+    check(e);
+    deleteItem(e);
+    edit(e);
+   });
+
+   _items.addEventListener("keydown", (e)=> {
+    commit(e);
+  });
   }
 
   function formHandler(e) {
     e.preventDefault();
-
-    console.log('halló heimur');
+    let val = document.querySelector(".form__input").value;
+    if(val.trim() != ""){ 
+    add(val);
+  }
+  document.querySelector(".form__input").value = "";
   }
 
-  // event handler fyrir það að klára færslu
-  function finish(e) {
+  function check(e) {
+    if(e.target.classList.contains("item__checkbox")){
+       e.target.parentNode.classList.toggle("item--done");
+   }
   }
 
-  // event handler fyrir það að breyta færslu
   function edit(e) {
+      if(e.target.classList.contains("item__text")){
+        let text = e.target.innerHTML;
+        let inp = el("input","item__edit",null,"text");
+        inp.value = text;
+        e.target.parentNode.replaceChild(inp, e.target);
+        inp.focus();
+        
+    }
   }
 
-  // event handler fyrir það að klára að breyta færslu
   function commit(e) {
+    let activeEl = document.activeElement;
+    if(activeEl.classList.contains("item__edit") && e.keyCode === ENTER_KEYCODE){
+      let text = activeEl.value;
+      let span = el("span","item__text",null);
+      span.innerHTML = text;
+      activeEl.parentNode.replaceChild(span,activeEl);
+    }
   }
 
-  // fall sem sér um að bæta við nýju item
   function add(value) {
+      var list =  el("list","item",null);
+      var checkb = el("input","item__checkbox",null,"checkbox");
+      var span = el("span","item__text",null);
+      var button = el("button","item__button",null);
+      button.innerHTML = "Eyða";
+      span.innerHTML = value;
+    
+      items.appendChild(list);
+      li.appendChild(checkb);
+      li.appendChild(span);
+      li.appendChild(button);
+       
   }
 
-  // event handler til að eyða færslu
   function deleteItem(e) {
+    if(e.target.classList.contains("item__button")){
+      e.target.parentNode.remove();
+    }
   }
 
-  // hjálparfall til að útbúa element
-  function el(type, className, clickHandler) {
+  function el(type, className, clickHandler, input = null) {
+    let elem  = document.createElement(type);
+    elem.classList.add(className);
+    if(input != null){
+      elem.setAttribute("type",input)
+    }
+    return elem;
   }
 
   return {
